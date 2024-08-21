d61f67093ddd8682551a9264b501825987991cfa
Somashekar B R <sanjayrao161@gmail.com>
2024-08-22 00:54:03 +0530

    theme ui v1 completed

--- script.js
+++ script.js
@@ -3,11 +3,18 @@ let id = 1;
 let todos = [];
 let editId = null;
 let search = "";
+
 function addTodo() {
+  if (search !== "") {
+    search = "";
+  }
+
   todos.push({
-    title: document.querySelector("#input-todo").value,
+    title: document.querySelector("#input-todo-title").value,
+    todo: document.querySelector("#input-todo").value,
     todo_id: "todo-" + id,
   });
+  document.querySelector("#input-todo-title").value = "";
   document.querySelector("#input-todo").value = "";
   id++;
   render();
@@ -23,19 +30,6 @@ function deleteTodo(todo_id) {
   render();
 }
 
-function render() {
-  const list = document.querySelector("#todo-list");
-  list.innerHTML = "";
-
-  if (todos.length > 0) {
-    let filtered = todos.filter((todo) =>
-      todo.title.toLowerCase().includes(search.toLowerCase())
-    );
-    filtered.forEach((todo) => {
-      list.appendChild(TodoComponent(todo));
-    });
-  }
-}
 function editTodo(id) {
   editId = id;
   render();
@@ -47,36 +41,63 @@ function updateTodo(id) {
     alert("Todo not exist");
     return;
   }
-  const value = document.querySelector("#edit-" + id).value;
-  todos[index].title = value;
+  const title = document.querySelector("#edit-title-" + id).value;
+  const todo = document.querySelector("#edit-todo-" + id).value;
+  todos[index].title = title;
+  todos[index].todo = todo;
   editId = null;
   render();
 }
 
 function findTodo() {
-  search = document.querySelector("#input-todo").value;
+  search = document.querySelector("#input-todo-title").value;
   render();
 }
 
-function TodoComponent({ title, todo_id }) {
+function TodoComponent({ title, todo, todo_id }) {
   const newTodo = document.createElement("div");
   newTodo.setAttribute("id", `todo-${todo_id}`);
   newTodo.setAttribute("class", `todo`);
   if (editId === todo_id) {
-    newTodo.innerHTML = `<input value="${title}" id="edit-${todo_id}"></input>
-    <button onClick="updateTodo('${todo_id}')">update</button>`;
+    //
+    newTodo.innerHTML = `
+    <input value="${title}" id="edit-title-${todo_id}" class="edit-input"></input>
+     <textarea value="${todo}" id="edit-todo-${todo_id}" class="edit-input">${todo}</textarea>
+    <div class="todo-button-cnt">
+      <button onClick="updateTodo('${todo_id}')" class="icon-button update">
+        <img class="icon"  src="https://img.icons8.com/?size=100&id=6Dz9xBnxD2Il&format=png&color=000000" />
+      </button>
+    </div>`;
+    //
   } else {
-    newTodo.innerHTML = `<h3>${title}</h3>
-    <div >
-    <button onClick="deleteTodo('${todo_id}')"  class="icon-button">
-    <img class="icon"  src="https://img.icons8.com/?size=100&id=4887&format=png&color=000000" />
-    </button>
-    <button onClick="editTodo('${todo_id}')" class="icon-button">
-    <img class="icon"  src="https://img.icons8.com/?size=100&id=15049&format=png&color=000000" />
-    </button>
+    //
+    newTodo.innerHTML = `<h3 class="todo-text">${title}</h3>
+    <p class="todo-text">${todo}</p>
+    <div class="todo-button-cnt" >
+      <button onClick="deleteTodo('${todo_id}')"  class="icon-button del">
+        <img class="icon"  src="https://img.icons8.com/?size=100&id=4887&format=png&color=000000" />
+      </button>
+      <button onClick="editTodo('${todo_id}')" class="icon-button edit">
+        <img class="icon"  src="https://img.icons8.com/?size=100&id=15049&format=png&color=000000" />
+      </button>
     </div>
     `;
   }
 
   return newTodo;
+}
+
+function render() {
+  const list = document.querySelector("#todo-list");
+  list.innerHTML = "";
+
+  if (todos.length > 0) {
+    let filtered = todos.filter((todo) =>
+      todo.title.toLowerCase().includes(search.toLowerCase())
+    );
+    // list.innerHTML = " <h1>Todos List</h1>";
+    filtered.forEach((todo) => {
+      list.appendChild(TodoComponent(todo));
+    });
+  }
 }
